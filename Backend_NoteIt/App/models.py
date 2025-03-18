8dff4e4b49f621da5e63fd6e9d07cada3cb1642c
unknown <krishmoorjani04@gmail.com>
2025-03-18 23:37:16 +0530

    Setup JWT Auth

--- Backend_NoteIt/App/models.py
+++ Backend_NoteIt/App/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+# Custom User Manager
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have a username")
+
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.set_password(password)  # Hashes the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(email, username, password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+# Custom User Model
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)  # Needed for Django Admin
+    is_superuser = models.BooleanField(default=False)  # Needed for Django Admin
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # Authentication via email
+    REQUIRED_FIELDS = ['username']  # 'password' is not needed here (handled by Django)
+
+    def __str__(self):
+        return self.email
--- Backend_NoteIt/App/views.py
+++ Backend_NoteIt/App/views.py
@@ -4,7 +4,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 class Home(APIView):
     authentication_classes = [JWTAuthentication]
